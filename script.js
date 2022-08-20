a6680c16cd4e27fce110a1622b368215675c863f
Adrian Perez <adrian1804perez@gmail.com>
2022-08-19 22:25:21 -0700

    Add grid and adjust style

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-
+let boxes = [];
 
 const grid = document.querySelector('.grid');
+
+for (i = 0; i < 256; i++){
+   boxes[i] = document.createElement('div');
+   boxes[i].classList.add('box');
+   grid.appendChild(boxes[i]);
+}
+
+
+
