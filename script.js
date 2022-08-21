794560ea958c9f1b1c164b6a7eef3d48f1d35f4d
Adrian Perez <adrian1804perez@gmail.com>
2022-08-21 08:49:04 -0700

    Add hovering effect for squares

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ let boxes = [];
 
 const grid = document.querySelector('.grid');
 
+function colors () {
+   boxes[i].style.backgroundColor = 'blue';
+}
 for (i = 0; i < 256; i++){
    boxes[i] = document.createElement('div');
    boxes[i].classList.add('box');
    grid.appendChild(boxes[i]);
+   boxes[i].addEventListener('mouseover', function (e) {
+      e.target.style.backgroundColor = 'blue';
+   });
 }
 
 
