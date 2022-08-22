5f5b72dbf9efbd5c67a85c8b6793e6b23484dd13
Adrian Perez <adrian1804perez@gmail.com>
2022-08-21 21:10:23 -0700

    Add button that changes grid size

--- script.js
+++ script.js
@@ -4,20 +4,37 @@ let gridNum = 16;
 let boxSize = 43.75;
 
 const grid = document.querySelector('.grid');
+const gridSizeBtn = document.querySelector('.question');
+
+gridSizeBtn.addEventListener('click',loadNewGrid);
+
+loadGrid();
 
 function colors () {
    boxes[i].style.backgroundColor = 'blue';
 }
-for (i = 0; i < gridSize; i++){
-   boxes[i] = document.createElement('div');
-   boxes[i].classList.add('box');
-   boxes[i].style.height = boxSize.toString() + "px";
-   boxes[i].style.width  = boxSize.toString() + "px";
-   grid.appendChild(boxes[i]);
-   boxes[i].addEventListener('mouseover', function (e) {
-      e.target.style.backgroundColor = 'blue';
+
+function loadNewGrid () {
+   getgridSize();
+   let oldBoxes = document.querySelectorAll('.box');
+   oldBoxes.forEach(thing => {
+      grid.removeChild(thing);
    });
+   loadGrid();
 }
+function loadGrid () {
+   for (i = 0; i < gridSize; i++){
+      boxes[i] = document.createElement('div');
+      boxes[i].classList.add('box');
+      boxes[i].style.height = boxSize.toString() + "px";
+      boxes[i].style.width  = boxSize.toString() + "px";
+      grid.appendChild(boxes[i]);
+      boxes[i].addEventListener('mouseover', function (e) {
+         e.target.style.backgroundColor = 'blue';
+      });
+   }
+}
+
 function getgridSize (){
    gridNum = prompt("How big would you like your grid? \(Enter how many squares you would like per side\)");
    gridSize = gridNum * gridNum;
