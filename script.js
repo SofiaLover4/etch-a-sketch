0c792e7bd1776493abc74bbe8e56cd55671c2da8
Adrian Perez <adrian1804perez@gmail.com>
2022-08-21 18:21:50 -0700

    Add getgridSize

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let boxes = [];
+let gridSize = 256;
+let gridNum = 16;
+let boxSize = 43.75;
 
 const grid = document.querySelector('.grid');
 
 function colors () {
    boxes[i].style.backgroundColor = 'blue';
 }
-for (i = 0; i < 256; i++){
+for (i = 0; i < gridSize; i++){
    boxes[i] = document.createElement('div');
    boxes[i].classList.add('box');
+   boxes[i].style.height = boxSize.toString() + "px";
+   boxes[i].style.width  = boxSize.toString() + "px";
    grid.appendChild(boxes[i]);
    boxes[i].addEventListener('mouseover', function (e) {
       e.target.style.backgroundColor = 'blue';
    });
 }
+function getgridSize (){
+   gridNum = prompt("How big would you like your grid? \(Enter how many squares you would like per side\)");
+   gridSize = gridNum * gridNum;
+   boxSize = Math.round(Math.sqrt(490000 / (gridNum * gridNum)) * 1000) / 1000; 
+}
+
+
 
 
 
